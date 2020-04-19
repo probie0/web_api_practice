1da9da25bbd826c3663a7c3e62c208079eabacea
probie <probie0@163.com>
2020-04-19 22:54:06 +0800

    update .gitignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # 用了python的fastapi框架
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 import uvicorn
@@ -14,6 +15,18 @@ class Task(BaseModel):
 
 
 app = FastAPI()
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/tasks")
@@ -29,10 +42,30 @@ async def add_task(task: Task):
     dict_task = jsonable_encoder(task)
     with open('data.json', 'r+') as f:
         tasks = json.load(f)
+        next_id = 1
+        if tasks:
+            next_id = max([t['id'] for t in tasks]) + 1
+        dict_task['id'] = next_id
         tasks.append(dict_task)
         f.seek(0)
+        f.truncate()
         json.dump(tasks, f)
         print("加载入文件完成...")
+
+
+@app.put("/api/tasks/{id}")
+async def modify(task: Task):
+    dict_task = jsonable_encoder(task)
+    id = dict_task['id']
+    with open('data.json', 'r+') as f:
+        tasks = json.load(f)
+        for i in range(len(tasks)):
+            if tasks[i]["id"] == id:
+                tasks[i] = dict_task
+                break
+        f.seek(0)
+        f.truncate()
+        json.dump(tasks, f)
 
 
 @app.get("/api/tasks/{id}")
